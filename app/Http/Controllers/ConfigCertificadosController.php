7056530889251574f4e9ed84da619859f8cae108
diegogomesmonteiro <diegogomes4@gmail.com>
2024-01-03 00:39:10 -0300

    ajuste ConfigCertificadosController

--- app/Http/Controllers/ConfigCertificadosController.php
+++ app/Http/Controllers/ConfigCertificadosController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Evento;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Enums\TipoConfigCertificadoEnum;
 use App\Models\Certificado\ConfigCertificado;
